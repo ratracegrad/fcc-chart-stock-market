c2678d6c959162c2693b24b9454efdea789b9887
Jennifer Bland <ratracegrad@gmail.com>
2016-05-01 17:18:38 -0400

    Added bower install for bootstrap, highcharts and jquery

---
+++ app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var dotenv = require('dotenv');
+
+var app = express();
+
+if (process.env.NODE_ENV !== 'test') {
+    app.use(logger('dev'));
+}
+dotenv.load({});
+
+app.use(favicon(path.join(__dirname, '../public/images', 'favicon.png')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('public'));
+
+
+module.exports = app;
---
+++ app/routes/healthcheck.js
@@ -0,0 +1,3 @@
+/**
+ * Created by jbland on 5/1/16.
+ */
