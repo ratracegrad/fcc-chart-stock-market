cd8481c640072d30009ad5f6ba117681a93a629a
Jennifer Bland <ratracegrad@gmail.com>
2016-04-30 20:15:40 -0400

    Added styling to sylesheet and add javascript file for application

--- public/javascripts/myScripts.js
+++ public/javascripts/myScripts.js
@@ -1,3 +1,29 @@
-/**
- * Created by jbland on 4/30/16.
- */
+'use strict';
+
+$(document).ready(function() {
+
+    //add click handlers to filter buttons
+    $('.stockFilter').click(function(event) {
+        event.preventDefault();
+
+        var stockSymbol = $('#stockName').text();
+
+        if (stockSymbol !== '') {
+            $.ajax('/' + this.id, {
+                method:     'POST',
+                dataType:   'json',
+                data:       JSON.stringify({ stockName: stockSymbol }),
+                success:    function(data) {
+                    console.log(data.dataset.name);
+                },
+                error:      function(jqXHR, statusString, err) {
+                    alert('Error: ' + err + '. Message: ' + statusString + '. Please try again.');
+                }
+            });
+        } else {
+            alert('Please enter a stock symbol first!');
+        }
+
+    });
+
+});
