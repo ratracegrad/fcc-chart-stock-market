9d2f6a215a88a22cdbf56416ed7bc5be3ea049a2
Jennifer Bland <ratracegrad@gmail.com>
2016-05-01 17:22:57 -0400

    Removed files that were no longer needed

--- public/javascripts/myScripts.js
+++ public/javascripts/myScripts.js
@@ -1,29 +1,150 @@
 'use strict';
 
-$(document).ready(function() {
 
-    //add click handlers to filter buttons
-    $('.stockFilter').click(function(event) {
-        event.preventDefault();
+function addstock() {
+    var seriesOptions = [];
+    var seriesCounter = 0;
+    var names = [];
 
-        var stockSymbol = $('#stockName').text();
+    var stockSymbol = $('#stockSymbol').val();
+    var currentStocks = $('.stockList').val();
+    if ( currentStocks !== '') {
+        names = currentStocks.split(',');
+        $('.stockList').val(currentStocks + ',' + stockSymbol);
+    } else {
+        $('.stockList').val(stockSymbol);
+    }
 
-        if (stockSymbol !== '') {
-            $.ajax('/' + this.id, {
-                method:     'POST',
-                dataType:   'json',
-                data:       JSON.stringify({ stockName: stockSymbol }),
-                success:    function(data) {
-                    console.log(data.dataset.name);
+    names.push(stockSymbol);
+    $('#stockSymbol').val(''); //blank out stock entry
+
+    function createChart() {
+        $('.stockChart').highcharts('StockChart', {
+
+            rangeSelector: {
+                selected: 4
+            },
+
+            yAxis: {
+                labels: {
+                    formatter: function () {
+                        return (this.value > 0 ? ' + ' : '') + this.value + '%';
+                    }
                 },
-                error:      function(jqXHR, statusString, err) {
-                    alert('Error: ' + err + '. Message: ' + statusString + '. Please try again.');
-                }
-            });
-        } else {
-            alert('Please enter a stock symbol first!');
-        }
+                plotLines: [{
+                    value: 0,
+                    width: 2,
+                    color: 'silver'
+                }]
+            },
 
+            plotOptions: {
+                series: {
+                    compare: 'percent'
+                }
+            },
+
+            tooltip: {
+                pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
+                valueDecimals: 2
+            },
+
+            series: seriesOptions
+        });
+    }
+
+    $.each(names, function (i, name) {
+
+        $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=' + name.toLowerCase() + '-c.json&callback=?',    function (data) {
+
+            seriesOptions[i] = {
+                name: name,
+                data: data
+            };
+
+            seriesCounter += 1;
+
+            if (seriesCounter === names.length) {
+                createChart();
+            }
+        });
     });
 
-});
+    displayStockList(names);
+}
+
+
+
+function displayStockList(stockList) {
+    $('#displayedStocks').empty();
+
+    $.each(stockList, function(i, stockSymbol) {
+        var html = '<div class="col-md-4 col-xs-6 oneStock"><h2>' + stockSymbol + '<button class="btn btn-default pull-right" onclick="deleteStock( ' + "'" + stockSymbol + "'" + ')">X</button></h2></div>';
+        $('#displayedStocks').append(html);
+    });
+
+}
+
+function deleteStock(whichStock) {
+    var seriesOptions = [];
+    var seriesCounter = 0;
+    var names = [];
+
+    var stockList = $('.stockList').val();
+    names = stockList.split(',');
+    var index = names.indexOf(whichStock);
+    names.splice(index, 1);
+    displayStockList(names);
+
+    function createChart() {
+        $('.stockChart').highcharts('StockChart', {
+
+            rangeSelector: {
+                selected: 4
+            },
+
+            yAxis: {
+                labels: {
+                    formatter: function () {
+                        return (this.value > 0 ? ' + ' : '') + this.value + '%';
+                    }
+                },
+                plotLines: [{
+                    value: 0,
+                    width: 2,
+                    color: 'silver'
+                }]
+            },
+
+            plotOptions: {
+                series: {
+                    compare: 'percent'
+                }
+            },
+
+            tooltip: {
+                pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
+                valueDecimals: 2
+            },
+
+            series: seriesOptions
+        });
+    }
+
+    $.each(names, function (i, name) {
+
+        $.getJSON('https://www.highcharts.com/samples/data/jsonp.php?filename=' + name.toLowerCase() + '-c.json&callback=?',    function (data) {
+
+            seriesOptions[i] = {
+                name: name,
+                data: data
+            };
+
+            seriesCounter += 1;
+
+            if (seriesCounter === names.length) {
+                createChart();
+            }
+        });
+    });
+}
